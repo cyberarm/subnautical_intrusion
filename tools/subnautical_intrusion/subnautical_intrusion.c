6c264bb34ed62b252d93306d3f393796ab707e8f
Cyberarm <matthewlikesrobots@gmail.com>
2025-06-01 22:52:36 -0500

    mrb_eval would be a better name...

--- tools/subnautical_intrusion/subnautical_intrusion.c
+++ tools/subnautical_intrusion/subnautical_intrusion.c
@@ -1,25 +1,14 @@
 #include <mruby.h>
+#include <mruby/compile.h>
 
 #ifdef MRB_NO_STDIO
 #error mruby-bin-mirb conflicts 'MRB_NO_STDIO' in your build configuration
 #endif
 
-#include <mruby/array.h>
-#include <mruby/proc.h>
-#include <mruby/compile.h>
-#include <mruby/dump.h>
-#include <mruby/string.h>
-#include <mruby/variable.h>
-#include <mruby/error.h>
-#include <mruby/presym.h>
-#include <mruby/internal.h>
-
 #include <stdlib.h>
 
 int main(int argc, char **argv)
 {
-  puts("LOADING...");
-
   mrb_state *mrb;
 
   mrb = mrb_open();
@@ -30,21 +19,7 @@ int main(int argc, char **argv)
     return EXIT_FAILURE;
   }
 
-  puts("LOADED MRB");
-
-  // mrb_eval(mrb, "SubnauticalIntrusion::Window.new(width: 1280, height: 800, resizable: true).show");
-  struct RClass *game_module, *game_window;
-  mrb_value instance;
-  puts("Retrieving game module...");
-  game_module = mrb_module_get(mrb, "SubnauticalIntrusion");
-  puts("Retrieving game window class...");
-  game_window = mrb_class_get_under(mrb, game_module, "Window");
-
-  puts("Creating instance of window...");
-  instance = mrb_class_new_instance(mrb, game_window, 0 , NULL);
-
-  puts("Spawning window...");
-  mrb_funcall(mrb, instance, "show", 0);
+  mrb_load_string(mrb, "SubnauticalIntrusion::Window.new(width: 1280, height: 800, resizable: true).show");
 
   mrb_close(mrb);
 
